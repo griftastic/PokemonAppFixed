ed5a9a269369cb1fbeb767ce80042af1dcea77a2
Becky <you@example.com>
2019-09-25 17:12:55 -0400

    first commit

---
+++ Pokemon_Repository/Pokemon_Repository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon_Repository
+{
+    public class PokemonRepository
+    {
+        List<Pokemon> _pokemonTeam = new List<Pokemon>();
+
+        //add to list (limit pokemon to 6)
+        public void AddPokemonToTeam(Pokemon pokemon)
+        {
+            if (_pokemonTeam.Count >= 0 && _pokemonTeam.Count < 6)
+            {
+                _pokemonTeam.Add(pokemon);
+            }
+        }
+        //get list
+        public List<Pokemon> GetPokemonTeam()
+        {
+            return _pokemonTeam;
+        }
+        //get one pokemon
+        public Pokemon GetPokemonByTeamPosition(int teamPosition)
+        {
+            Pokemon pokemon = _pokemonTeam[teamPosition - 1];
+            return pokemon;
+        }
+        //update pokemon
+        public void UpdatePokemonByTeamPosition(int teamPosition, Pokemon updates)
+        {
+            Pokemon pokemon = _pokemonTeam[teamPosition - 1];
+            pokemon.PokemonSpeciesName = updates.PokemonSpeciesName;
+            pokemon.PokemonNickName = updates.PokemonNickName;
+            pokemon.Level = updates.Level;
+            pokemon.PokemonType = updates.PokemonType;
+            pokemon.SecondaryType = updates.SecondaryType;
+            pokemon.MoveOne = updates.MoveOne;
+            pokemon.MoveTwo = updates.MoveTwo;
+            pokemon.MoveThree = updates.MoveThree;
+            pokemon.MoveFour = updates.MoveFour;
+        }
+        public void ChangeTeamPositionForAPokemon(int originalPosition, int newPosition)
+        {
+            Pokemon pokemon = _pokemonTeam[originalPosition];
+            _pokemonTeam.Insert(newPosition, pokemon);
+        }
+        //remove pokemon
+        public void RemovePokemonFromTeam(int teamPosition)
+        {
+            Pokemon pokemon = _pokemonTeam[teamPosition - 1];
+            _pokemonTeam.Remove(pokemon);
+        }
+    }
+}
